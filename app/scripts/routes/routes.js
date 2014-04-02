8186d74cd7af3f132c18f87d968696f7a6821110
Christian Kessler IV <me@cgk.io>
2014-04-01 21:28:52 -0400

    comming

--- app/scripts/routes/routes.js
+++ app/scripts/routes/routes.js
@@ -38,11 +38,27 @@ angular.module('myApp')
     // GOALS STATES AND NESTED VIEWS ========================================
     .state('goals', {
         url: '/goals',
-        templateUrl: 'partials/goals.html'
+        templateUrl: 'partials/goals.html',
+        controller: 'goalsCtrl'
+    })
+
+    // nested list with custom controller
+    .state('goals.inbed', {
+        url: '/inbed',
+        templateUrl: 'partials/goals-inbed.html',
+        controller: function($scope) {
+            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+        }
     })
 
     // PROJECTS STATES AND NESTED VIEWS ========================================
     .state('projects', {
+        url: '/projects',
+        templateUrl: 'partials/projects.html'
+    })
+
+    // PROJECTS-DETAILS STATES AND NESTED VIEWS ========================================
+    .state('projects/', {
         url: '/projects',
         templateUrl: 'partials/projects.html'
     })
