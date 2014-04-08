673e9c92dbeab46b005c2a12cb86ebd5dc28ead3
Christian Kessler IV <me@cgk.io>
2014-04-08 00:12:49 -0400

    commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,5 @@
 'use strict';
 
 angular.module('myApp', [
-    'ui.router',
-    'ngResource',
-    'myApp.services'
+  'ui.router'
 ]);
--- scripts/services/myservice.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('aoclabsApp')
-  .service('Myservice', function Myservice() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
