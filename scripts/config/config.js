96d1ef74010555df94008e29d79d8a7e2c8e7a04
Christian Kessler IV <me@cgk.io>
2014-06-11 00:33:23 -0400

    Auto-generated commit by grunt-gh-pages

--- scripts/config/config.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-// Declare app level module which depends on filters, and services
-angular.module('myApp.config', []).constant('version', '0.6').constant('loginRedirectPath', '/login').constant('FBURL', 'https://cgkio.firebaseio.com');
