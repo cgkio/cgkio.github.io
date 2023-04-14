63148fe87ce2e77d49c129fa29719f07c077bab9
Christian Kessler IV <cgkio@users.noreply.github.com>
2023-04-14 15:47:23 -0400

    staging files

---
+++ script.js
@@ -0,0 +1,98 @@
+window.addEventListener("load", function () {
+  const doorPositions = [
+    { x: 670, y: 650 }, // Door1
+    { x: 705, y: 650 }, // Door2
+    { x: 850, y: 650 }, // Door3
+    { x: 885, y: 650 }, // Door4
+    { x: 1060, y: 650 }, // Door5
+    { x: 1095, y: 650 }, // Door6
+    { x: 1235, y: 650 }, // Door7
+    { x: 1270, y: 650 }, // Door8
+    { x: 660, y: 743 }, // Door9
+    { x: 705, y: 743 }, // Door10
+    { x: 850, y: 743 }, // Door11
+    { x: 885, y: 743 }, // Door12
+    { x: 1050, y: 743 }, // Door13
+    { x: 1085, y: 743 }, // Door14
+    { x: 1235, y: 743 }, // Door15
+    { x: 1270, y: 743 }, // Door16
+  ];
+
+  // Initialize doors and set their positions
+  doorPositions.forEach((position, index) => {
+    try {
+      const door = document.getElementById(`door${index + 1}`);
+      door.style.left = `${position.x}px`;
+      door.style.top = `${position.y}px`;
+    } catch (error) {
+      console.error(`Error in door${index + 1}:`, error);
+    }
+  });
+
+  const doorMsgPositions = [
+    { x: 630, y: 563 }, // Door1
+    { x: 675, y: 563 }, // Door2
+    { x: 810, y: 563 }, // Door3
+    { x: 855, y: 563 }, // Door4
+    { x: 1020, y: 563 }, // Door5
+    { x: 1065, y: 563 }, // Door6
+    { x: 1195, y: 563 }, // Door7
+    { x: 1240, y: 563 }, // Door8
+    { x: 630, y: 835 }, // Door9
+    { x: 675, y: 835 }, // Door10
+    { x: 810, y: 835 }, // Door11
+    { x: 855, y: 835 }, // Door12
+    { x: 1010, y: 835 }, // Door13
+    { x: 1055, y: 835 }, // Door14
+    { x: 1195, y: 835 }, // Door15
+    { x: 1240, y: 835 }, // Door16
+  ];
+
+  // Initialize turnaround time messages and set their positions
+  doorMsgPositions.forEach((position, index) => {
+    try {
+      const doorMsg = document.getElementById(`door${index + 1}Msg`);
+      doorMsg.style.left = `${position.x}px`;
+      doorMsg.style.top = `${position.y}px`;
+    } catch (error) {
+      console.error(`Error in doorMsg${index + 1}:`, error);
+    }
+  });
+
+  // Function to update door color based on status
+  function updateDoorColor(doorNumber, status) {
+    const door = document.getElementById(`door${doorNumber}`);
+    door.style.backgroundColor = status ? "red" : "green";
+    if (status) {
+      door.classList.remove("green-door");
+    } else {
+      door.classList.add("green-door");
+    }
+  }
+
+  // Function to update door message status
+  function updateDoorMsg(doorNumber, status) {
+    document.getElementById(`door${doorNumber}Msg`).innerHTML = "<div class='smalltext'>Last Reported Turnaround</div>" + status;
+  }
+
+  // Connect to Firebase Realtime Database
+  const database = firebase.database();
+
+  // Listen for changes in door statuses
+  for (let i = 1; i <= 16; i++) {
+    const doorRef = firebase.database().ref(`doors/Door${i}`);
+    doorRef.on("value", (snapshot) => {
+      const status = snapshot.val();
+      updateDoorColor(i, status);
+    });
+  }
+
+  // Listen for changes in door message statuses
+  for (let i = 1; i <= 16; i++) {
+    const doorMsgRef = firebase.database().ref(`doorsMsg/Door${i}Msg`);
+    doorMsgRef.on("value", (snapshot) => {
+      const status = snapshot.val();
+      updateDoorMsg(i, status);
+    });
+  }
+});
